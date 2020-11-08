2822bc8d305f6d4e67b199a11c015d10625ff29f
william <wjrichards@sky.com>
2020-11-08 20:40:27 +0000

    unit test update

--- test/test_queries.py
+++ test/test_queries.py
@@ -1,6 +1,4 @@
 import unittest
-import os, sys
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app.routes import app, database
 from app.queries import customers, aggregated_metrics, get_data
 
@@ -20,13 +18,15 @@ class TestApi(unittest.TestCase):
         with app.app_context():
             self.get_data_response = get_data(self.session, date)
             self.aggregated_metrics_response = aggregated_metrics(self.session, date)
-        import pdb; pdb.set_trace()
+            self.customer_response = customers(self.session, date)
 
     def test_endpoint_analytics(self):
         """
-        Testing the database functions'.
+        Testing the database functions'. Assert statements that must be true.
         """
         assert self.get_data_response.status_code == 200, "Wrong status code."
         assert self.get_data_response.json != {}, "Empty response"
         assert len(self.aggregated_metrics_response) == 6, "incorrect query response length"
         assert self.aggregated_metrics_response[3] >= 0, "total discount must be >= 0"
+        assert self.customer_response >= 0, "customers must be an integer that's > 0"
+        assert type(self.customer_response) == int, "customers must be an integer"
